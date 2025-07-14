9b0b18a28b6e0d65c07ad73f7465002e0ac227fb
CyrilH714 <cyril.j.haller@gmail.com>
2025-07-14 16:05:35 +0100

    implement login signup and authorisation, also admin

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
-
-
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from .models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Define the home view function
 class Home(LoginView):
@@ -9,4 +14,26 @@ class Home(LoginView):
     template_name = 'home.html'
 
 # Create your views here.
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'body', 'tags']
+    def form_valid(self, form):
+        form.instance.user = self.request.user  
+        return super().form_valid(form)
 
+
+def signup(request):
+    error_message=''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
+
+# @login_required 
